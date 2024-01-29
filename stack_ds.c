9679c5156da19752e2b6fd700e150923ab9db23b
Abdulrahman Hassan Al-Sayed <adrehmanhsn@hotmail.com>
2024-01-29 17:28:01 +0200

    Update stack_ds.c

--- stack_ds.c
+++ stack_ds.c
@@ -1,9 +1,18 @@
+/**
+ **************************************************************************************
+ * This File Contains Stack_t Function.
+ * For More Information Return To The stack_ds.h file .
+ **************************************************************************************
+ */
 
+// ==================================== Header files ===================================
 #include "stack_ds.h"
 
-static stack_status_t(stack_full(stack_ds_t *the_stack))
+// =================================== Implementation ==================================
+
+static stack_status_t(stack_full(stack_t *stack_obj))
 {
-    if(STACK_MAX_SIZE-1 == the_stack->stack_pointer)
+    if(STACK_MAX_SIZE-1 == stack_obj->stack_pointer)
     {
         return STACK_FULL;
     }else
@@ -12,9 +21,10 @@ static stack_status_t(stack_full(stack_ds_t *the_stack))
     }
 }
 
-static stack_status_t(stack_empty(stack_ds_t *the_stack))
+// ---------------------------------------------------------------------------------------
+static stack_status_t(stack_empty(stack_t *stack_obj))
 {
-    if(the_stack->stack_pointer == -1)
+    if(stack_obj->stack_pointer == -1)
     {
         return STACK_EMPTY;
     }else
@@ -23,28 +33,29 @@ static stack_status_t(stack_empty(stack_ds_t *the_stack))
     }
 }
 
-
-return_status_t stack_init(stack_ds_t *the_stack)
+// ---------------------------------------------------------------------------------------
+return_status_t stack_init(stack_t *stack_obj)
 {
     return_status_t ret = R_NOK;
-    if(NULL == the_stack)
+    if(NULL == stack_obj)
     {
         ret = R_NOK;
     }
     else
     {
-        the_stack->stack_pointer = -1;
+        stack_obj->stack_pointer = -1;
         ret = R_OK;
     }
 
     return ret;
 }
 
-return_status_t stack_push(stack_ds_t *the_stack , uint32_t value)
+// ---------------------------------------------------------------------------------------
+return_status_t stack_push(stack_t *stack_obj , uint32_t value)
 {
     return_status_t ret = R_NOK;
 
-    if((NULL == the_stack) || (STACK_FULL == stack_full(the_stack)))
+    if((NULL == stack_obj) || (STACK_FULL == stack_full(stack_obj)))
     {
         #ifdef STACK_DEBUG
         printf("Error !! Stack Is Full Can\'t Push [%i]. \n",value);
@@ -52,8 +63,8 @@ return_status_t stack_push(stack_ds_t *the_stack , uint32_t value)
         ret = R_NOK;
     }else
     {
-        the_stack->stack_pointer ++;
-        the_stack->data[the_stack->stack_pointer] = value;
+        stack_obj->stack_pointer ++;
+        stack_obj->data[stack_obj->stack_pointer] = value;
         #ifdef STACK_DEBUG
         printf("Value [%i] Pushed To Stack .\n",value);
         #endif
@@ -63,10 +74,11 @@ return_status_t stack_push(stack_ds_t *the_stack , uint32_t value)
     return ret;
 }
 
-return_status_t stack_pop(stack_ds_t *the_stack , uint32_t *value)
+// ---------------------------------------------------------------------------------------
+return_status_t stack_pop(stack_t *stack_obj , uint32_t *value)
 {
     return_status_t ret = R_NOK;
-    if((NULL == the_stack) || (NULL == value) || (STACK_EMPTY == stack_empty(the_stack)))
+    if((NULL == stack_obj) || (NULL == value) || (STACK_EMPTY == stack_empty(stack_obj)))
     {
         #ifdef STACK_DEBUG
         printf("Error !! Stack Is Empty Can\'t Pop Any Value.\n");
@@ -74,8 +86,8 @@ return_status_t stack_pop(stack_ds_t *the_stack , uint32_t *value)
         ret = R_NOK;
     }else
     {
-        *value = the_stack->data[the_stack->stack_pointer];
-        the_stack->stack_pointer --;
+        *value = stack_obj->data[stack_obj->stack_pointer];
+        stack_obj->stack_pointer --;
         #ifdef STACK_DEBUG
         printf("Value [%i] Pop From Stack .\n",*value);
         #endif
@@ -84,10 +96,11 @@ return_status_t stack_pop(stack_ds_t *the_stack , uint32_t *value)
     return ret;
 }
 
-return_status_t stack_top(stack_ds_t *the_stack , uint32_t *value)
+// ---------------------------------------------------------------------------------------
+return_status_t stack_top(stack_t *stack_obj , uint32_t *value)
 {
     return_status_t ret = R_NOK;
-    if((NULL == the_stack) || (NULL == value) || (STACK_EMPTY == stack_empty(the_stack)))
+    if((NULL == stack_obj) || (NULL == value) || (STACK_EMPTY == stack_empty(stack_obj)))
     {
         #ifdef STACK_DEBUG
         printf("Error !! Stack Is Empty Can\'t Pop Any Value.\n");
@@ -95,7 +108,7 @@ return_status_t stack_top(stack_ds_t *the_stack , uint32_t *value)
         ret = R_NOK;
     }else
     {
-        *value = the_stack->data[the_stack->stack_pointer];
+        *value = stack_obj->data[stack_obj->stack_pointer];
         #ifdef STACK_DEBUG
         printf("Value Of the StackTop [%i].\n",*value);
         #endif
@@ -104,3 +117,47 @@ return_status_t stack_top(stack_ds_t *the_stack , uint32_t *value)
 
     return ret ;
 }
+
+// ---------------------------------------------------------------------------------------
+return_status_t stack_size(stack_t *stack_obj, uint32_t *value)
+{
+    return_status_t ret = R_NOK;
+    if((NULL == stack_obj) || (NULL == value))
+    {
+        #ifdef STACK_DEBUG
+        printf("Error !! Stack OR Value Is Not Valid Pointer");
+        #endif // STACK_DEBUG
+        ret = R_NOK;
+    }
+    else
+    {
+        *value = (stack_obj->stack_pointer +1);
+        ret = R_OK;
+    }
+
+    return ret;
+}
+
+// ---------------------------------------------------------------------------------------
+return_status_t stack_display(stack_t *stack_obj)
+{
+    return_status_t ret = R_NOK;
+    sint32_t l_itrator = ZERO_INT;
+    if((NULL == stack_obj) || (STACK_EMPTY == stack_empty(stack_obj)))
+    {
+        #ifdef STACK_DEBUG
+        printf("Error !! Stack Pointer Is Not Valid OR Stack Is Empty");
+        #endif // STACK_DEBUG
+        ret = R_NOK;
+    }else
+    {
+        printf("\n[");
+        for(l_itrator = stack_obj->stack_pointer;l_itrator >= ZERO; l_itrator--)
+        {
+            printf(" %i,",stack_obj->data[l_itrator]);
+        }
+        printf("]\n");
+        ret = R_OK;
+    }
+}
+
